7ba6984291da6d3e6c0801b54a5e2827bbf27bf1
Ian Fragoso <IanPaul@Ians-MacBook-Pro.local>
2019-12-04 20:24:30 -0700

    declared routes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
+import { Route, Link } from "react-router-dom"
+
 const App = () => (
   <div>
     <Navigation />
+    <Route path="/" Component={Home}/>
+    <Route path="/about" Component={About}/>
+    <Route path="/contact" Component={Contact}/>
   </div>
 );
 
